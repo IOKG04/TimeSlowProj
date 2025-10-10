1f700b120affd10397bd3a277b48c37e03a3e3ea
IOKG04 <iokg04@gmail.com>
2025-10-11 00:50:19 +0200

    gaussian blur :3
    this branch is less to actually be merged some day,
    more just so I remember how to do this.
    
    before I tackle shaders for real, Imma make sprites lul

--- src/game.zig
+++ src/game.zig
@@ -17,6 +17,11 @@ pub var camera: raylib.Camera2D = .{
     .zoom = @as(f32, @floatFromInt(options.window_h)) / 8.0,
 };
 
+var primary_render_target: raylib.RenderTexture2D = undefined;
+var gaussian_horizontal: raylib.Shader = undefined;
+var gaussian_horizontal_target: raylib.RenderTexture2D = undefined;
+var gaussian_vertical: raylib.Shader = undefined;
+
 pub fn init(gpa: Allocator) !void {
     // init raylib
     raylib.initWindow(options.window_w, options.window_h, "TimeSlowProj");
@@ -32,9 +37,23 @@ pub fn init(gpa: Allocator) !void {
         errdefer player.game_object.deinit(&player.game_object, gpa);
         try scene.addGameObject(gpa, &player.game_object);
     }
+
+    primary_render_target = try raylib.loadRenderTexture(options.window_w, options.window_h);
+    errdefer primary_render_target.unload();
+    gaussian_horizontal = try raylib.loadShader(null, "resources/shaders/gaussian_horizontal.glsl");
+    errdefer gaussian_horizontal.unload();
+    gaussian_horizontal_target = try raylib.loadRenderTexture(options.window_w, options.window_h);
+    errdefer gaussian_horizontal_target.unload();
+    gaussian_vertical = try raylib.loadShader(null, "resources/shaders/gaussian_vertical.glsl");
+    errdefer gaussian_vertical.unload();
 }
 
 pub fn close(gpa: Allocator) void {
+    primary_render_target.unload();
+    gaussian_horizontal.unload();
+    gaussian_horizontal_target.unload();
+    gaussian_vertical.unload();
+
     scene.deinit(gpa);
     raylib.closeWindow();
 }
@@ -45,11 +64,39 @@ pub fn run(gpa: Allocator) !void {
         try scene.update(gpa, dt, player.game_object.transform.position, -0.1);
 
         // zig fmt: off
-        raylib.beginDrawing();
+        primary_render_target.begin();
             raylib.clearBackground(.black);
             camera.begin();
                 scene.draw();
             camera.end();
+        primary_render_target.end();
+
+        gaussian_horizontal_target.begin();
+            gaussian_horizontal.activate();
+                primary_render_target.texture.drawRec(.{
+                    .x = 0,
+                    .y = 0,
+                    .width = @floatFromInt(primary_render_target.texture.width),
+                    .height = @floatFromInt(-primary_render_target.texture.height),
+                }, .{
+                    .x = 0,
+                    .y = 0,
+                }, .white);
+            gaussian_horizontal.deactivate();
+        gaussian_horizontal_target.end();
+
+        raylib.beginDrawing();
+            gaussian_vertical.activate();
+                gaussian_horizontal_target.texture.drawRec(.{
+                    .x = 0,
+                    .y = 0,
+                    .width = @floatFromInt(gaussian_horizontal_target.texture.width),
+                    .height = @floatFromInt(-gaussian_horizontal_target.texture.height),
+                }, .{
+                    .x = 0,
+                    .y = 0,
+                }, .white);
+            gaussian_vertical.deactivate();
             raylib.drawFPS(0, 0);
         raylib.endDrawing();
         // zig fmt: on
